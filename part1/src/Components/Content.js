cc0e7f0022c9ab4380a150429c25299c1bd5b7f7
cmerchant <canaan.merchant@vexterra.com>
2019-06-04 10:41:01 -0400

    updating components

---
+++ part1/src/Components/Content.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Content = () => {
+  const parts = [ {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+   {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+    {
+    name: 'State of a component',
+    exercises: 14,
+    }
+]
+
+export default app;
---
+++ part1/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = () => {
+    const course = 'Half Stack application development'
+
+    return(
+      <div>
+        <h1> {course} </h1>
+      </div>
+    )
+}
+
+export default Header;
