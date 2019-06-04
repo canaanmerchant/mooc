a6e1eafefb818948a51bb6e7f330c64fc8d9569a
cmerchant <canaan.merchant@vexterra.com>
2019-06-04 16:34:27 -0400

    working out content.js problem

--- part1/src/Components/Content.js
+++ part1/src/Components/Content.js
@@ -2,19 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const Content = () => {
-  const parts = [ {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-   {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-    {
-    name: 'State of a component',
-    exercises: 14,
-    }
-]
 
-export default app;
+const Content = (props) => {
+  return (
+    <div>
+      <Part {props.part1} {props.exercises1} />
+      <Part {props.part2} {props.exercises2} />
+      <Part {props.part3} {props.exercises3} />
+    </div>
+  )
+}
+
+{/* const Content = (props) => {
+
+
+
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+
+    </div>
+  )
+} */}
+
+export default Content;
--- part1/src/Components/Header.js
+++ part1/src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = () => {
-    const course = 'Half Stack application development'
-
+const Header = (props) => {
+  console.log(props)
+  
     return(
       <div>
-        <h1> {course} </h1>
+        <h1> {props.course} </h1>
       </div>
     )
 }
